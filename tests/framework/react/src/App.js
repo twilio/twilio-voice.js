4aac56b1672a5f20aeb56392463e62edf976bd6e
mmalavalli <mmalavalli@twilio.com>
2023-06-08 11:40:53 -0700

    VBLOCKS-1301 WIP: adding mjs export module.

--- tests/framework/react/src/App.js
+++ tests/framework/react/src/App.js
@@ -4,19 +4,19 @@ import { Device } from 'twilio-client';
 export default class App extends Component {
   constructor(props) {
     super(props);
-
-    this.state = {};
-
-    Device.ready(() => {
-      this.setState({ success: true });
-    });
-    
-    Device.setup(props.token)
+    const device = new Device(props.token);
+    device.on('error', () => this.setState({ ...this.state, success: false }));
+    device.on('registered', () => this.setState({ ...this.state, success: true }));
+    device.register().catch(() => this.setState({ ...this.state, success: false }));
+    this.state = { device };
   }
 
   render() {
-    if (this.state.success) {
+    if (this.state.success === true) {
       return <p>Setup successful</p>;
+    }
+    if (this.state.success === false) {
+      return <p>Setup failed</p>;
     }
     return <p>Calling Device.setup</p>;
   }
