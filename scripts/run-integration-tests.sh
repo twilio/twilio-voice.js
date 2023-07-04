e40c5ee333fcaa4de4f76e0ccd1f880aff7603b9
charliesantos <csantos@twilio.com>
2023-07-04 10:09:31 -0700

    VBLOCKS-1818 | Adding healthchecks and posting failures to slack

---
+++ scripts/run-integration-tests.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+function reportFailure() {
+  if [ -n "${SLACK_WEBHOOK}" ] && [ -n "${BUILD_LABEL}" ]
+  then
+    BRANCH_NAME=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+    SLACK_MESSAGE=":failed_build: ${BUILD_LABEL} ${BROWSER} ${BVER} on branch ${BRANCH_NAME}"
+    echo "Reporting to slack: ${SLACK_MESSAGE}"
+    curl -X POST -H 'Content-type: application/json' --data '{"text": '\""$SLACK_MESSAGE"\"'}' $SLACK_WEBHOOK
+  fi
+
+  echo "Integration tests failed"
+  exit 1
+}
+
+./node_modules/.bin/karma start $PWD/karma.conf.ts --log-level debug && echo "Integration tests passed" || reportFailure
