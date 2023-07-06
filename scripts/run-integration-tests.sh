ef29b3a1c91a735236d6e659a62e24629f34c5d2
charliesantos <csantos@twilio.com>
2023-07-06 09:40:43 -0700

    Adding build url

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 function reportFailure() {
-  if [ -n "${SLACK_WEBHOOK}" ] && [ -n "${BUILD_LABEL}" ]
+  if [ -n "${SLACK_WEBHOOK}" ] && [ -n "${BUILD_LABEL}" ] && [ -n "${CIRCLE_BUILD_URL}" ]
   then
     BRANCH_NAME=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-    SLACK_MESSAGE=":failed_build: ${BUILD_LABEL} ${BROWSER} ${BVER} on branch ${BRANCH_NAME}"
+    SLACK_MESSAGE=":failed_build: <${CIRCLE_BUILD_URL}|${BUILD_LABEL}> ${BROWSER} ${BVER} on branch ${BRANCH_NAME}"
     echo "Reporting to slack: ${SLACK_MESSAGE}"
     curl -X POST -H 'Content-type: application/json' --data '{"text": '\""$SLACK_MESSAGE"\"'}' $SLACK_WEBHOOK
   fi
