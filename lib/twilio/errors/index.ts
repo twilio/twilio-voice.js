4d2867897e518c8af70f18a516cbfb1d95b4d334
Michael Huynh <12516091+mhuynh5757@users.noreply.github.com>
2023-10-03 14:20:32 -0700

    [VBLOCKS-2054] Missing generated errors (#192)
    * feat: add errors and unit tests to check for missing errors
    
    * docs: changelog entry
    
    * fix: call hangup handler
    
    * docs: update changelog
    
    * feat: add error feature flag and handling
    
    * chore: unit tests
    
    * chore: update voice errors version
    
    * feat: add new errors to generation scripts
    
    * chore: commit generated errors
    
    * fix: add error codes to feature flag set
    
    * docs: update changelog entry
    
    * docs: update docstring for option
    
    * docs: less enthusiasm
    
    * fix: rename variables and functions
    
    * fix: rename test case

--- lib/twilio/errors/index.ts
+++ lib/twilio/errors/index.ts
@@ -8,11 +8,82 @@ import {
   ClientErrors,
   errorsByCode,
   GeneralErrors,
+  MalformedRequestErrors,
   MediaErrors,
   SignalingErrors,
+  SignatureValidationErrors,
+  SIPServerErrors,
   TwilioError,
   UserMediaErrors,
 } from './generated';
+
+/**
+ * NOTE(mhuynh): Replacing generic error codes with new (more specific) codes,
+ * is a breaking change. If an error code is found in this set, we only perform
+ * the transformation if the feature flag is enabled.
+ *
+ * With every major version bump, such that we are allowed to introduce breaking
+ * changes as per semver specification, this array should be cleared.
+ *
+ * TODO: [VBLOCKS-2295] Remove this in 3.x
+ */
+const PRECISE_SIGNALING_ERROR_CODES: Set<number> = new Set([
+  /**
+   * 310XX Errors
+   */
+  31001,
+  31002,
+  31003,
+  /**
+   * 311XX Errors
+   */
+  31101,
+  31102,
+  31103,
+  31104,
+  31105,
+  31107,
+  /**
+   * 312XX Errors
+   */
+  31201,
+  31202,
+  31203,
+  31204,
+  31205,
+  31207,
+  /**
+   * 314XX Errors
+   */
+  31404,
+  31480,
+  31486,
+  /**
+   * 316XX Errors
+   */
+  31603,
+]);
+export function getPreciseSignalingErrorByCode(
+  enableImprovedSignalingErrorPrecision: boolean,
+  errorCode: number,
+): typeof TwilioError | undefined {
+  if (typeof errorCode !== 'number') {
+    return;
+  }
+
+  if (!hasErrorByCode(errorCode)) {
+    return;
+  }
+
+  const shouldTransform = enableImprovedSignalingErrorPrecision
+    ? true
+    : !PRECISE_SIGNALING_ERROR_CODES.has(errorCode);
+  if (!shouldTransform) {
+    return;
+  }
+
+  return getErrorByCode(errorCode);
+}
 
 // Application errors that can be avoided by good app logic
 export class InvalidArgumentError extends Error {
@@ -50,13 +121,24 @@ export function hasErrorByCode(code: number): boolean {
   return errorsByCode.has(code);
 }
 
-// All errors we want to throw or emit locally in the SDK need to be passed through here.
+/**
+ * All errors we want to throw or emit locally in the SDK need to be passed
+ * through here.
+ *
+ * They need to first be defined in the `USED_ERRORS` list. See:
+ * ```
+ * scripts/errors.js
+ * ```
+ */
 export {
   AuthorizationErrors,
   ClientErrors,
   GeneralErrors,
+  MalformedRequestErrors,
   MediaErrors,
   SignalingErrors,
+  SignatureValidationErrors,
+  SIPServerErrors,
   TwilioError,
   UserMediaErrors,
 };
--- tests/unit/error.ts
+++ tests/unit/error.ts
@@ -1,14 +1,13 @@
 import * as assert from 'assert';
-import { getErrorByCode, MediaErrors, TwilioError } from '../../lib/twilio/errors';
+import * as errors from '../../lib/twilio/errors';
 
-/* tslint:disable-next-line */
-describe('Errors', function() {
+describe('Errors', () => {
   describe('constructor', () => {
     it('should use message', () => {
-      const error: TwilioError = new MediaErrors.ConnectionError('foobar');
+      const error = new errors.MediaErrors.ConnectionError('foobar');
       assert(error instanceof Error);
-      assertTwilioError(error);
-      assert(error instanceof MediaErrors.ConnectionError);
+      assert(error instanceof errors.TwilioError);
+      assert(error instanceof errors.MediaErrors.ConnectionError);
       assert.equal(error.code, 53405);
       assert.equal(error.message, 'ConnectionError (53405): foobar');
       assert.equal(error.originalError, undefined);
@@ -16,10 +15,10 @@ describe('Errors', function() {
 
     it('should use originalError', () => {
       const originalError = new Error('foobar');
-      const error: TwilioError = new MediaErrors.ConnectionError(originalError);
+      const error = new errors.MediaErrors.ConnectionError(originalError);
       assert(error instanceof Error);
-      assertTwilioError(error);
-      assert(error instanceof MediaErrors.ConnectionError);
+      assert(error instanceof errors.TwilioError);
+      assert(error instanceof errors.MediaErrors.ConnectionError);
       assert.equal(error.code, 53405);
       assert.equal(error.message, 'ConnectionError (53405): Raised by the Client or Server whenever a media connection fails.');
       assert.equal(error.originalError, originalError);
@@ -27,20 +26,20 @@ describe('Errors', function() {
 
     it('should use both message and originalError', () => {
       const originalError = new Error('foobar');
-      const error: TwilioError = new MediaErrors.ConnectionError('foobar', originalError);
+      const error = new errors.MediaErrors.ConnectionError('foobar', originalError);
       assert(error instanceof Error);
-      assertTwilioError(error);
-      assert(error instanceof MediaErrors.ConnectionError);
+      assert(error instanceof errors.TwilioError);
+      assert(error instanceof errors.MediaErrors.ConnectionError);
       assert.equal(error.code, 53405);
       assert.equal(error.message, 'ConnectionError (53405): foobar');
       assert.equal(error.originalError, originalError);
     });
 
     it('should allow no params', () => {
-      const error: TwilioError = new MediaErrors.ConnectionError();
+      const error = new errors.MediaErrors.ConnectionError();
       assert(error instanceof Error);
-      assertTwilioError(error);
-      assert(error instanceof MediaErrors.ConnectionError);
+      assert(error instanceof errors.TwilioError);
+      assert(error instanceof errors.MediaErrors.ConnectionError);
       assert.equal(error.code, 53405);
       assert.equal(error.message, 'ConnectionError (53405): Raised by the Client or Server whenever a media connection fails.');
       assert.equal(error.originalError, undefined);
@@ -49,21 +48,59 @@ describe('Errors', function() {
 
   describe('getErrorByCode', () => {
     it('should throw if code is not found', () => {
-      assert.throws(() => getErrorByCode(123456));
+      assert.throws(() => errors.getErrorByCode(123456));
     });
 
     it('should return the TwilioError if code is found', () => {
-      const twilioError: any = getErrorByCode(53405);
-      const error: TwilioError = new twilioError();
+      const twilioError: any = errors.getErrorByCode(53405);
+      const error = new twilioError();
       assert(error instanceof Error);
-      assertTwilioError(error);
-      assert(error instanceof MediaErrors.ConnectionError);
+      assert(error instanceof errors.TwilioError);
+      assert(error instanceof errors.MediaErrors.ConnectionError);
       assert.equal(error.code, 53405);
     });
   });
-});
 
-// Currently the only way to check an interface at runtime.
-function assertTwilioError(error: Error): error is TwilioError {
-  return true;
-}
+  it('generated error map matches subset of errors in scripts', () => {
+    const USED_ERRORS: string = require('../../scripts/errors').USED_ERRORS;
+    for (const errorFullName of USED_ERRORS) {
+      const [namespace, name] = errorFullName.split('.');
+      const errorConstructor = (errors as any)[namespace][name];
+      assert(typeof errorConstructor === 'function');
+    }
+  });
+
+  describe('getPreciseSignalingErrorByCode', () => {
+    it('should return a constructor when using the feature flag and the error is behind the flag', () => {
+      const errorConstructor = errors.getPreciseSignalingErrorByCode(true, 31480);
+      if (typeof errorConstructor !== 'function') {
+        throw new Error('error constructor should be defined');
+      }
+      const error = new errorConstructor();
+      assert.equal(error.code, 31480);
+    });
+
+    it('should return undefined when not using the feature flag and the error is behind the flag', () => {
+      const errorConstructor = errors.getPreciseSignalingErrorByCode(false, 31480);
+      assert.equal(typeof errorConstructor, 'undefined');
+    });
+
+    it('should return a constructor when using the feature flag and the error is not behind the flag', () => {
+      const errorConstructor = errors.getPreciseSignalingErrorByCode(true, 31009);
+      if (typeof errorConstructor !== 'function') {
+        throw new Error('error constructor should be defined');
+      }
+      const error = new errorConstructor();
+      assert.equal(error.code, 31009);
+    });
+
+    it('should return a constructor when not using the feature flag and the error is not behind the flag', () => {
+      const errorConstructor = errors.getPreciseSignalingErrorByCode(false, 31009);
+      if (typeof errorConstructor !== 'function') {
+        throw new Error('error constructor should be defined');
+      }
+      const error = new errorConstructor();
+      assert.equal(error.code, 31009);
+    });
+  });
+});
