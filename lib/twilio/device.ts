ebc13d174ac1ff3175ca95e0c5066e1d1f4c6a5b
Michael Huynh <mhuynh@twilio.com>
2023-10-02 20:14:29 -0700

    docs: update docstring for option

--- lib/twilio/device.ts
+++ lib/twilio/device.ts
@@ -1734,7 +1734,67 @@ namespace Device {
     edge?: string[] | string;
 
     /**
-     * Granular error codes.
+     * Enhance the precision of errors emitted by `Device` and `Call` objects.
+     *
+     * The default value of this option is `false`.
+     *
+     * When this flag is enabled, some errors that would have been described
+     * with a generic error code, namely `53000` and `31005`, are now described
+     * with a more precise error code. With this feature, the following errors
+     * now have their own error codes. Please see this
+     * [page](https://www.twilio.com/docs/api/errors) for more details about
+     * each error.
+     *
+     * - Device Error Changes
+     *
+     * @example
+     * ```ts
+     * const device = new Device(token, {
+     *   enableImprovedSignalingErrorPrecision: true,
+     * });
+     * device.on('error', (deviceError) => {
+     *   // the following table describes how deviceError will change with this feature flag
+     * });
+     * ```
+     *
+     * | Device Error Name | Device Error Code with Feature Flag Enabled | Device Error Code with Feature Flag Disabled |
+     * | --- | --- | --- |
+     * | `GeneralErrors.ApplicationNotFoundError` | `31001` | `53000` |
+     * | `GeneralErrors.ConnectionDeclinedError` | `31002` | `53000` |
+     * | `GeneralErrors.ConnectionTimeoutError` | `31003` | `53000` |
+     * | `MalformedRequestErrors.MissingParameterArrayError` | `31101` | `53000` |
+     * | `MalformedRequestErrors.AuthorizationTokenMissingError` | `31102` | `53000` |
+     * | `MalformedRequestErrors.MaxParameterLengthExceededError` | `31103` | `53000` |
+     * | `MalformedRequestErrors.InvalidBridgeTokenError` | `31104` | `53000` |
+     * | `MalformedRequestErrors.InvalidClientNameError` | `31105` | `53000` |
+     * | `MalformedRequestErrors.ReconnectParameterInvalidError` | `31107` | `53000` |
+     * | `SignatureValidationErrors.AccessTokenSignatureValidationFailed` | `31202` | `53000` |
+     * | `AuthorizationErrors.NoValidAccountError` | `31203` | `53000` |
+     * | `AuthorizationErrors.JWTTokenExpirationTooLongError` | `31207` | `53000` |
+     * | `ClientErrors.NotFound` | `31404` | `53000` |
+     * | `ClientErrors.TemporarilyUnavilable` | `31480` | `53000` |
+     * | `ClientErrors.BusyHere` | `31486` | `53000` |
+     * | `SIPServerErrors.Decline` | `31603` | `53000` |
+     *
+     * - Call Error Changes
+     *
+     * @example
+     * ```ts
+     * const device = new Device(token, {
+     *   enableImprovedSignalingErrorPrecision: true,
+     * });
+     * const call = device.connect(...);
+     * call.on('error', (callError) => {
+     *   // the following table describes how callError will change with this feature flag
+     * });
+     * ```
+     *
+     * | Call Error Name | Call Error Code with Feature Flag Enabled | Call Error Code with Feature Flag Disabled |
+     * | --- | --- | --- |
+     * | `GeneralErrors.ConnectionDeclinedError` | `31002` | `31005` |
+     * | `AuthorizationErrors.InvalidJWTTokenError` | `31204` | `31005` |
+     * | `AuthorizationErrors.JWTTokenExpiredError` | `31205` | `31005` |
+     *
      */
     enableImprovedSignalingErrorPrecision?: boolean;
 
