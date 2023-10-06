895aa3de44ec6bdcef427191b8d6672f2df9b9c5
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2023-10-06 17:56:11 +0000

    2.7.4-dev

--- esm/twilio/rtc/sample.js
+++
@@ -1,7 +0,0 @@
-/**
- * @packageDocumentation
- * @module Voice
- * @publicapi
- */
-export {};
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic2FtcGxlLmpzIiwic291cmNlUm9vdCI6IiIsInNvdXJjZXMiOlsiLi4vLi4vLi4vbGliL3R3aWxpby9ydGMvc2FtcGxlLnRzIl0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBOzs7O0dBSUcifQ==
