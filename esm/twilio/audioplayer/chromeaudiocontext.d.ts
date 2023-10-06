b8353823feda49244f1dd1d290cb94ee4fcfd1a0
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2023-10-06 17:56:02 +0000

    2.7.3

---
+++ esm/twilio/audioplayer/chromeaudiocontext.d.ts
@@ -0,0 +1,15 @@
+/**
+ * @packageDocumentation
+ * @module Voice
+ * @internalapi
+ */
+export default interface ChromeAudioContext extends AudioContext {
+    createMediaStreamDestination: () => any;
+    destination: MediaStreamAudioDestinationNode;
+}
+export interface ChromeHTMLAudioElement extends HTMLAudioElement {
+    setSinkId: (sinkId: string) => Promise<void>;
+}
+export interface MediaStreamAudioDestinationNode extends AudioDestinationNode {
+    stream: MediaStream;
+}
