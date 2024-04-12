34e8afb28f3b52b32605ae5e8bdf857088d6cc25
charliesantos <csantos@twilio.com>
2024-04-12 09:34:34 -0700

    VBLOCKS-2928 Chrome Extension MV3 App

---
+++ extension/README.md
@@ -0,0 +1,10 @@
+## Twilio Voice JS SDK Chrome Extension Manifest V3 Example Application
+This project demonstrates how to use the Twilio Voice JS SDK in a Chrome Extension Manifest V3 (MV3) application. If you are not familiar with MV3, please check out the official [documentation](https://developer.chrome.com/docs/extensions/develop/migrate/what-is-mv3) for more details.
+
+This project has a server component that provides the Voice Access Token, and the Chrome Extension example app which can be loaded on the Chrome browser. See the diagram below for more details on the call flow.
+
+### Incoming Call
+![incoming-sequence](https://github.com/twilio/twilio-voice.js/assets/22135968/a5e3d03a-53db-49b9-948b-692cd568860d)
+
+### Outgoing Call
+![outgoing-sequence](https://github.com/twilio/twilio-voice.js/assets/22135968/31687569-a106-497d-b7b2-82e4a71e2f7f)
---
+++ extension/app/popup/popup.js
@@ -0,0 +1,69 @@
+let initBtn;
+let callBtn;
+let hangupBtn;
+let acceptBtn;
+let rejectBtn;
+let recepientEl;
+let statusEl;
+
+function start() {
+  recepientEl = document.querySelector('#recepient');
+  statusEl = document.querySelector('#status');
+  initBtn = document.querySelector('#init');
+  callBtn = document.querySelector('#call');
+  hangupBtn = document.querySelector('#hangup');
+  acceptBtn = document.querySelector('#accept');
+  rejectBtn = document.querySelector('#reject');
+
+  setupButtonHandlers();
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.type === 'setstate') {
+      setStatus(request.state.status);
+    }
+  });
+  chrome.runtime.sendMessage({ type: 'getstate' }, ({ status }) => {
+    setStatus(status);
+  });
+
+  // Connect to the service worker. This allows the service worker
+  // to detect whether the popup is open or not.
+  chrome.runtime.connect();
+}
+
+function setStatus(status) {
+  if (status === 'pending') {
+    showButtons('init');
+  } else if (status === 'idle') {
+    showButtons('call');
+  } else if (status === 'incoming') {
+    showButtons('accept', 'reject');
+  } else if (status === 'inprogress') {
+    showButtons('hangup');
+  }
+  recepientEl.style.display = status === 'idle' ? 'block' : 'none';
+  statusEl.innerHTML = `Status: ${status}`;
+}
+
+function setupButtonHandlers() {
+  initBtn.onclick = () => chrome.runtime.sendMessage({ type: 'init' });
+  callBtn.onclick = () => recepientEl.value && chrome.runtime.sendMessage({ type: 'call', recepient: recepientEl.value });
+  hangupBtn.onclick = () => chrome.runtime.sendMessage({ type: 'hangup' });
+  acceptBtn.onclick = () => chrome.runtime.sendMessage({ type: 'accept' });
+  rejectBtn.onclick = () => chrome.runtime.sendMessage({ type: 'reject' });
+}
+
+function showButtons(...buttonsToShow) {
+  document.querySelectorAll('button').forEach(el => {
+    if (buttonsToShow.includes(el.id)) {
+      el.style.display = 'inline-block';
+    } else {
+      el.style.display = 'none';
+    }
+  });
+}
+
+function log(msg) {
+  document.querySelector('#log').innerHTML += msg + '\n';
+}
+
+start();
