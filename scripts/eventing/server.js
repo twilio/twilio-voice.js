9ec99e6464e068645768d94deb1b0ea0d969b486
Michael Huynh <mhuynh@twilio.com>
2024-05-15 13:54:59 -0700

    feat: eventing integration tests happy paths

---
+++ scripts/eventing/server.js
@@ -0,0 +1,191 @@
+// @ts-check
+
+'use strict';
+
+const ngrok = require('@ngrok/ngrok');
+const axios = require('axios').default;
+const express = require('express');
+const cors = require('cors');
+
+/** @type {Map<string, any[]>} */
+const receivedMessages = new Map();
+
+/**
+ * @returns {{
+ *    apiKeySid: string;
+ *    apiKeySecret: string;
+ *    accountSid: string;
+ *    eventingTwimlAppSid: string;
+ * }}
+ */
+const getEnvVars = () => {
+  const apiKeySid = process.env['API_KEY_SID'];
+  if (typeof apiKeySid !== 'string') {
+    throw new Error('API_KEY_SID env var not defined.');
+  }
+
+  const apiKeySecret = process.env['API_KEY_SECRET'];
+  if (typeof apiKeySecret !== 'string') {
+    throw new Error('API_KEY_SECRET env var not defined.');
+  }
+
+  const accountSid = process.env['ACCOUNT_SID'];
+  if (typeof accountSid !== 'string') {
+    throw new Error('ACCOUNT_SID env var not defined.');
+  }
+
+  const eventingTwimlAppSid = process.env['EVENTING_TWIML_APP_SID'];
+  if (typeof eventingTwimlAppSid !== 'string') {
+    throw new Error('EVENTING_TWIML_APP_SID env var not defined.');
+  }
+
+  return { apiKeySid, apiKeySecret, accountSid, eventingTwimlAppSid };
+};
+
+/**
+ * @typedef {(callSid: string, callbackUrl: string) => Promise<import('axios').AxiosResponse>} CreateSubscription
+ */
+
+/**
+ * @typedef {(callSid: string) => Promise<import('axios').AxiosResponse>} SendMessage
+ */
+
+/**
+ * @param {string} accountSid
+ * @param {string} apiKeySid
+ * @param {string} apiKeySecret
+ */
+const createTwilioUserDefinedMessageActions = (accountSid, apiKeySid, apiKeySecret) => {
+  const baseUrl = 'https://api.twilio.com/2010-04-01';
+  const extendedUrl = `${baseUrl}/Accounts/${accountSid}/Calls`;
+
+  /**
+   * @type {CreateSubscription}
+   */
+  const createSubscription = (callSid, callbackUrl) => {
+    const url =
+      `${extendedUrl}/${callSid}/UserDefinedMessageSubscriptions.json`;
+
+    const params = new URLSearchParams();
+    params.append('Method', 'POST');
+    params.append('Callback', callbackUrl);
+
+    return axios.post(url, params, {
+      auth: {
+        username: apiKeySid,
+        password: apiKeySecret,
+      },
+    });
+  };
+
+  /**
+   * @type {SendMessage}
+   */
+  const sendMessage = (callSid) => {
+    const url =
+      `${extendedUrl}/${callSid}/UserDefinedMessages.json`;
+
+    const params = new URLSearchParams();
+    params.append('Content', JSON.stringify({ message: 'ahoy, world!' }));
+
+    return axios.post(url, params, {
+      auth: {
+        username: apiKeySid,
+        password: apiKeySecret,
+      },
+    });
+  };
+
+  return { createSubscription, sendMessage };
+}
+
+/**
+ * @param {CreateSubscription} createSubscription
+ * @param {SendMessage} sendMessage
+ */
+const createExpressApp = (createSubscription, sendMessage) => {
+  const app = express();
+
+  app.use(cors());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  app.post('/create-subscription', async (req, res) => {
+    const { CallSid } = req.body;
+    if (typeof CallSid !== 'string') {
+      res.sendStatus(400);
+      throw new Error('CallSid is not of type string');
+    }
+    const callbackUrl = `${app.listener.url()}/receive-message`;
+    console.log('creating a subscription', { CallSid, callbackUrl });
+
+    const subscription = await createSubscription(CallSid, callbackUrl);
+    console.log(subscription.data);
+
+    res.json(subscription.data);
+  });
+
+  app.post('/receive-message', (req, res) => {
+    console.log('received message', req.body);
+
+    const { CallSid } = req.body;
+    if (typeof CallSid !== 'string') {
+      res.sendStatus(400);
+      throw new Error('CallSid is not of type string');
+    }
+
+    const existingMessages = receivedMessages.get(CallSid) || [];
+    receivedMessages.set(CallSid, [...existingMessages, req.body]);
+
+    res.sendStatus(200);
+  });
+
+  app.post('/get-received-messages', (req, res) => {
+    console.log('sending received messages', req.body);
+
+    const { CallSid } = req.body;
+    if (typeof CallSid !== 'string') {
+      res.sendStatus(400);
+      throw new Error('CallSid is not of type string');
+    }
+
+    res.json(receivedMessages.get(CallSid));
+  });
+
+  app.post('/send-message', async (req, res) => {
+    const { CallSid } = req.body;
+    if (typeof CallSid !== 'string') {
+      res.sendStatus(400);
+      throw new Error('CallSid is not of type string');
+    }
+
+    const sendMessageResponse = await sendMessage(CallSid);
+    console.log('sent message', sendMessageResponse.data);
+
+    res.json(sendMessageResponse.data);
+  });
+
+  return app;
+};
+
+const start = async () => {
+  const envVars = getEnvVars();
+
+  const { createSubscription, sendMessage } =
+    createTwilioUserDefinedMessageActions(
+      envVars.accountSid,
+      envVars.apiKeySid,
+      envVars.apiKeySecret,
+    );
+  const app = createExpressApp(createSubscription, sendMessage);
+
+  await ngrok.listen(app);
+
+  console.log(`listening on ${app.listener.url()}`);
+
+  app.listen(3030, () => {
+    console.log('listening on 3030');
+  });
+};
+
+start().catch(console.error);
---
+++ tests/integration/eventing.ts
@@ -0,0 +1,126 @@
+import * as assert from 'assert';
+import axios from 'axios';
+import Device from '../../lib/twilio/device';
+import { generateAccessToken } from '../lib/token';
+const env = require('../env');
+
+const RELAY_SERVER_URL = 'http://localhost:3030';
+
+describe('eventing', function() {
+  this.timeout(1000 * 60 * 10); // 10 minute timeout for the whole suite
+
+  describe('outgoing call', function() {
+    const createOutgoingCallTest = async () => {
+      const token = generateAccessToken(
+        `client-id-eventing-tests-outgoing-call-${Date.now()}`,
+        60 * 3, // 3 minute TTL
+        env.eventingTwimlAppSid,
+      );
+      const device = new Device(token);
+      const call = await device.connect();
+      await new Promise((res) => {
+        console.log('waiting for call to be accepted');
+        call.on('accept', () => {
+          console.log('call accepted');
+          res();
+        });
+      });
+      return { device, call };
+    };
+
+    it(
+      'should successfully send a message to the customer server',
+      async function() {
+        const { device, call } = await createOutgoingCallTest();
+
+        const CallSid = call.parameters.CallSid;
+        console.log('call sid', CallSid);
+        const subscription = await axios.post(
+          `${RELAY_SERVER_URL}/create-subscription`,
+          { CallSid },
+        );
+        console.log(`subscription made for call ${CallSid}`, subscription.data);
+
+        const eventSid = call.sendMessage({
+          content: { hello: 'world' },
+          messageType: 'user-defined-message',
+        });
+        await new Promise((res, rej) => {
+          console.log('waiting for twilio to receive the message')
+          call.once('messageSent', (msg) => {
+            if (msg.voiceEventSid === eventSid) {
+              return res();
+            }
+            rej();
+          });
+        });
+        console.log('sent message', eventSid);
+
+        // NOTE(mhuynh): Using a quick solution here such that we just wait for
+        // the message from Twilio to reach the test server. Otherwise, there is
+        // a high likelihood that the message will be missed.
+        await new Promise((res) => {
+          setTimeout(() => {
+            res();
+          }, 3000);
+        });
+
+        const receivedMessagesResponse = await axios.post(
+          `${RELAY_SERVER_URL}/get-received-messages`,
+          { CallSid },
+        );
+        const receivedMessages = receivedMessagesResponse.data;
+        console.log('received messages', receivedMessages);
+
+        assert.strictEqual(typeof receivedMessages, 'object');
+        assert(
+          Array.isArray(receivedMessages),
+          'received messages is not of type array',
+        );
+        assert.strictEqual(receivedMessages.length, 1);
+
+        const [receivedMessage] = receivedMessages
+        assert.strictEqual(receivedMessage.ContentType, 'application/json');
+        assert.strictEqual(receivedMessage.Content, JSON.stringify({
+          hello: 'world',
+        }));
+        assert.strictEqual(receivedMessage.SequenceNumber, '1');
+        assert.strictEqual(receivedMessage.CallSid, CallSid);
+        assert.strictEqual(receivedMessage.Sid, eventSid);
+
+        device.destroy();
+      },
+    );
+
+    it('should successfully receive an incoming message', async function() {
+      const { device, call } = await createOutgoingCallTest();
+
+      const messageReceivedPromise = new Promise<any>((res) => {
+        call.on('messageReceived', (msg) => {
+          res(msg);
+        });
+      });
+
+      const CallSid = call.parameters.CallSid;
+      const sendMessageResponse = await axios.post(
+        `${RELAY_SERVER_URL}/send-message`,
+        { CallSid },
+      );
+      const sentMessage = sendMessageResponse.data;
+      console.log('message sent by server', sentMessage);
+
+      const receivedMessage = await messageReceivedPromise;
+      console.log('message received by client', receivedMessage);
+
+      assert.strictEqual(receivedMessage.voiceEventSid, sentMessage.sid);
+      assert.deepStrictEqual(
+        receivedMessage.content,
+        { message: 'ahoy, world!' },
+      );
+      assert.strictEqual(receivedMessage.contentType, 'application/json');
+      assert.strictEqual(receivedMessage.messageType, 'user-defined-message');
+
+      device.destroy();
+    });
+  });
+});
