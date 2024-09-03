5b4c80d4e989d9bc9f670ebbd326875190489e03
charliesantos <csantos@twilio.com>
2024-09-03 13:48:19 -0700

    tests

--- tests/unit/cdn/viewerrequest/test.js
+++ tests/unit/cdn/viewerrequest/test.js
@@ -25,25 +25,28 @@ describe('CDN Viewer Request', () => {
       testUri: '/js/video/releases/2.28.1/docs/',
       expectedUri: '/js/video/releases/2.28.1/docs/index.html',
     },
-    {
-      testUri: '/sdk/js/client/v1.15/twilio.js',
-      expectedUri: '/js/client/v1.15/twilio.js',
-    },{
-      testUri: '/sdk/js/client/releases/1.15.1/twilio.js',
-      expectedUri: '/js/client/releases/1.15.1/twilio.js',
-    },{
-      testUri: '/sdk/js/video/latest/docs',
-      expectedUri: '/js/video/latest/docs',
-    },{
-      testUri: '/sdk/js/video/latest/docs/',
-      expectedUri: '/js/video/latest/docs',
-    },{
-      testUri: '/sdk/js/video/releases/2.28.1/docs',
-      expectedUri: '/js/video/releases/2.28.1/docs',
-    },{
-      testUri: '/sdk/js/video/releases/2.28.1/docs/',
-      expectedUri: '/js/video/releases/2.28.1/docs',
-    }
+    ...
+    [
+      {
+        testUri: '/sdk/js/client/v1.15/twilio.js',
+        expectedUri: '/js/client/v1.15/twilio.js',
+      },{
+        testUri: '/sdk/js/client/releases/1.15.1/twilio.js',
+        expectedUri: '/js/client/releases/1.15.1/twilio.js',
+      },{
+        testUri: '/sdk/js/video/latest/docs',
+        expectedUri: '/js/video/latest/docs',
+      },{
+        testUri: '/sdk/js/video/latest/docs/',
+        expectedUri: '/js/video/latest/docs',
+      },{
+        testUri: '/sdk/js/video/releases/2.28.1/docs',
+        expectedUri: '/js/video/releases/2.28.1/docs',
+      },{
+        testUri: '/sdk/js/video/releases/2.28.1/docs/',
+        expectedUri: '/js/video/releases/2.28.1/docs',
+      }
+    ]
   ].forEach(({ testUri, expectedUri, unmodified }) => {
     it(testUri, () => {
       const result = viewerRequestHandler({ request: { uri: testUri }});
