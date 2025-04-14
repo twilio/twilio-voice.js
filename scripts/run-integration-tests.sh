df66e1ec96e9fd1a86568e2f404db6bd18b99031
Kevin <kchoy@twilio.com>
2025-04-14 13:55:36 -0700

    add else to script

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if [ $1 == "chrome" ]; then
+if [[ "$1" == "chrome" ]]; then
   npm run test:cypress -- browser /usr/bin/google-chrome
-elif [ $1 === "firefox" ]; then
+elif [[ "$1" === "firefox" ]]; then
   npm run test:cypress -- browser /usr/local/bin/firefox
+else
+  echo "$1 browser not supported"
+  exit 1
 fi
