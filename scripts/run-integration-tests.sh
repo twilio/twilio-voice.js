87ff8a2a0496d0a44e87d75d02d9a61a0cd5af3e
Kevin <kchoy@twilio.com>
2025-04-14 14:03:21 -0700

    nit

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -2,7 +2,7 @@
 
 if [[ "$1" == "chrome" ]]; then
   npm run test:cypress -- browser /usr/bin/google-chrome
-elif [[ "$1" === "firefox" ]]; then
+elif [[ "$1" == "firefox" ]]; then
   npm run test:cypress -- browser /usr/local/bin/firefox
 else
   echo "$1 browser not supported"
