681777e618cc8caa41c9f4c63bb3a7042c4d41ba
Kevin Choy <kevinchoy22@gmail.com>
2025-06-17 15:24:54 -0700

    nit: remove spec argument for firefox

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -5,7 +5,7 @@ if [[ "$1" == "chrome" && -n "$2" ]]; then
 elif [[ "$1" == "chrome" ]]; then
   npm run test:integration:chrome && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
 elif [[ "$1" == "firefox" ]]; then
-  npm run test:integration -- --spec 'cypress/e2e/**/*.cy.ts' --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
+  npm run test:integration -- --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
 else
   echo "$1 browser not supported"
   exit 1
