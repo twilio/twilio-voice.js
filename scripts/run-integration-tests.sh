8ec61a3219d5b0a1b87cf5fd20cfbe640280ef4c
Kevin Choy <35968892+kpchoy@users.noreply.github.com>
2025-06-17 16:06:32 -0700

    [VBLOCKS-4841] Fix: healthcheck (#345)
    * fix: healthcheck
    
    * chore: always upload dd
    
    * refactor: integration script, cypress config
    
    * refactor: script
    
    * nit: remove spec argument for firefox
    
    * nit: specify chrome

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if [[ "$1" == "chrome" ]]; then
+if [[ "$1" == "chrome" && -n "$2" ]]; then
+  npm run test:integration:chrome -- --spec "$2" && echo "$2 tests passed" || bash scripts/report-failure.sh "$2 tests"
+elif [[ "$1" == "chrome" ]]; then
   npm run test:integration:chrome && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
 elif [[ "$1" == "firefox" ]]; then
   npm run test:integration -- --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
