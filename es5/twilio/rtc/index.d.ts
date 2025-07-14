b95a6300452cd38a46e8e20c1bc349f695c0349f
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2025-07-14 18:52:51 +0000

    2.15.1-dev

--- es5/twilio/rtc/index.d.ts
+++
@@ -1,4 +0,0 @@
-import PeerConnection from './peerconnection';
-declare function enabled(): boolean;
-declare function getMediaEngine(): "ORTC" | "WebRTC";
-export { enabled, getMediaEngine, PeerConnection, };
