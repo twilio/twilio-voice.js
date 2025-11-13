b5dcd79f37cb359e42199315ed52d32467ef631c
Kevin Choy <35968892+kpchoy@users.noreply.github.com>
2025-11-13 13:48:40 -0800

    [VBLOCKS-5532] feat: remote audio processor (#377)
    * feat: remote audio processor
    
    * refactor: address comments
    
    * fix: createaudiooutput
    
    * refactor: peer connection
    
    * refactor: setaudiosource promise
    
    * refactor: peerconnection
    
    * fix: onaddtrack fallback use default device
    
    * refactor: handleaudioprocessorevent

--- lib/twilio/audioprocessoreventobserver.ts
+++ lib/twilio/audioprocessoreventobserver.ts
@@ -13,19 +13,20 @@ export class AudioProcessorEventObserver extends EventEmitter {
   constructor() {
     super();
     this._log.info('Creating AudioProcessorEventObserver instance');
-    this.on('enabled', () => this._reEmitEvent('enabled'));
-    this.on('add', () => this._reEmitEvent('add'));
-    this.on('remove', () => this._reEmitEvent('remove'));
-    this.on('create', () => this._reEmitEvent('create-processed-stream'));
-    this.on('destroy', () => this._reEmitEvent('destroy-processed-stream'));
+
+    this.on('enabled', (isRemote) => this._reEmitEvent('enabled', isRemote));
+    this.on('add', (isRemote) => this._reEmitEvent('add', isRemote));
+    this.on('remove', (isRemote) => this._reEmitEvent('remove', isRemote));
+    this.on('create', (isRemote) => this._reEmitEvent('create-processed-stream', isRemote));
+    this.on('destroy', (isRemote) => this._reEmitEvent('destroy-processed-stream', isRemote));
   }
 
   destroy(): void {
     this.removeAllListeners();
   }
 
-  private _reEmitEvent(name: string): void {
-    this._log.info(`AudioProcessor:${name}`);
-    this.emit('event', { name, group: 'audio-processor' });
+  private _reEmitEvent(name: string, isRemote: boolean): void {
+    this._log.info(`${isRemote ? 'Remote' : 'Local'} AudioProcessor:${name}`);
+    this.emit('event', { name, group: 'audio-processor', isRemote });
   }
 }
