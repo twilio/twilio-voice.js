baee657308053050baab57c45d7f23215e5f25d4
Kevin Choy <kevinchoy22@gmail.com>
2026-01-14 09:34:10 -0800

    refactor: integration script

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -5,15 +5,15 @@ if [ "$CIRCLECI" = "true" ]; then
   TESTFILES=$(circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)
   TEST_LIST=$(echo $TESTFILES | tr ' ' ',')
   echo $TEST_LIST
-fi
 
-if [[ "$1" == "chrome" && -n "$2" ]]; then
-  npm run test:integration:chrome -- --spec "$2" && echo "$2 tests passed" || bash scripts/report-failure.sh "$2 tests"
-elif [[ "$1" == "chrome" ]]; then
-  npm run test:integration:chrome -- --spec "$TEST_LIST" && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
-elif [[ "$1" == "firefox" ]]; then
-  npm run test:integration -- --spec "$TEST_LIST" --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
-else
-  echo "$1 browser not supported"
-  exit 1
+  if [[ "$1" == "chrome" && -n "$2" ]]; then
+    npm run test:integration:chrome -- --spec "$2" && echo "$2 tests passed" || bash scripts/report-failure.sh "$2 tests"
+  elif [[ "$1" == "chrome" ]]; then
+    npm run test:integration:chrome -- --spec "$TEST_LIST" && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
+  elif [[ "$1" == "firefox" ]]; then
+    npm run test:integration -- --spec "$TEST_LIST" --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
+  else
+    echo "$1 browser not supported"
+    exit 1
+  fi
 fi
