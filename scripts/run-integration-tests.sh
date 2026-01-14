b673deffc9878331e0b0a8ae84ca4463744a79f0
Kevin Choy <35968892+kpchoy@users.noreply.github.com>
2026-01-14 10:32:30 -0800

    [VBLOCKS-5841] feat: parallelize healthchecks (#395)
    * feat: parallelize healthchecks
    
    * fix: glob
    
    * refactor: script
    
    * feat: add store_test_results
    
    * fix: remove list
    
    * refactor: respond comments
    
    * nit
    
    * nit
    
    * only execute subset
    
    * nit
    
    * test_list
    
    * 6 containers
    
    * refactor: integration script
    
    * nit: 4 containers

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-if [[ "$1" == "chrome" && -n "$2" ]]; then
-  npm run test:integration:chrome -- --spec "$2" && echo "$2 tests passed" || bash scripts/report-failure.sh "$2 tests"
-elif [[ "$1" == "chrome" ]]; then
-  npm run test:integration:chrome && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
-elif [[ "$1" == "firefox" ]]; then
-  npm run test:integration -- --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
-else
-  echo "$1 browser not supported"
-  exit 1
+if [ "$CIRCLECI" = "true" ]; then
+  echo "Asking circleci to pick tests based on timings..."
+  TESTFILES=$(circleci tests glob "cypress/e2e/**/*.cy.ts" | circleci tests split --split-by=timings)
+  TEST_LIST=$(echo $TESTFILES | tr ' ' ',')
+  echo $TEST_LIST
+
+  if [[ "$1" == "chrome" && -n "$2" ]]; then
+    npm run test:integration:chrome -- --spec "$2" && echo "$2 tests passed" || bash scripts/report-failure.sh "$2 tests"
+  elif [[ "$1" == "chrome" ]]; then
+    npm run test:integration:chrome -- --spec "$TEST_LIST" && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
+  elif [[ "$1" == "firefox" ]]; then
+    npm run test:integration -- --spec "$TEST_LIST" --browser /usr/local/bin/firefox && echo "Integration tests passed" || bash scripts/report-failure.sh "Integration tests"
+  else
+    echo "$1 browser not supported"
+    exit 1
+  fi
 fi
