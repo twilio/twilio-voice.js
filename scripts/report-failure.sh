8e97a688bc13a09e8cf4ba5ecc880d0e158d122b
Kevin Choy <kevinchoy22@gmail.com>
2026-03-17 10:54:06 -0700

    chore: slack only report after auto-rerun fail and not for pr workflow

--- scripts/report-failure.sh
+++ scripts/report-failure.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ -n "${SLACK_WEBHOOK}" ] && [ -n "${BUILD_LABEL}" ] && [ -n "${CIRCLE_BUILD_URL}" ] && [ -n "$1" ]
+BROWSER_ARG="$1"
+TEST_FILES_ARG="$2"
+
+if [ -n "${TEST_FILES_ARG}" ]; then
+  STEP_NAME="${TEST_FILES_ARG} tests"
+elif [ -n "${BROWSER_ARG}" ]; then
+  STEP_NAME="Integration tests (${BROWSER_ARG})"
+else
+  STEP_NAME="Integration tests"
+fi
+
+if [ -n "${SLACK_WEBHOOK}" ] && [ -n "${BUILD_LABEL}" ] && [ -n "${CIRCLE_BUILD_URL}" ] && [ "${IS_PR_WORKFLOW}" != "true" ]
 then
   BRANCH_NAME=$(git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
-  SLACK_MESSAGE=":failed_build: <${CIRCLE_BUILD_URL}|${BUILD_LABEL}> ${BROWSER} ${BVER} on branch ${BRANCH_NAME} on step $1"
+  SLACK_MESSAGE=":failed_build: <${CIRCLE_BUILD_URL}|${BUILD_LABEL}> ${BROWSER} ${BVER} on branch ${BRANCH_NAME} on step ${STEP_NAME}"
   echo "Reporting to slack: ${SLACK_MESSAGE}"
   curl -X POST -H 'Content-type: application/json' --data '{"text": '\""$SLACK_MESSAGE"\"'}' $SLACK_WEBHOOK
 fi
 
-echo "Step failed: $1"
+echo "Step failed: ${STEP_NAME}"
 
 exit 1
