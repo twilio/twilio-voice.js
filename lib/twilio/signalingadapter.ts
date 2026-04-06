ed4077871ec5ae34bf601a4ce6d81f16bab8ebc7
Luis Rivas <lrivas@twilio.com>
2026-04-06 13:54:25 -0500

    feat: add SignalingAdapter interface

---
+++ lib/twilio/signalingadapter.ts
@@ -0,0 +1,30 @@
+import { EventEmitter } from 'events';
+
+export type SignalingAdapterStatus = 'disconnected' | 'connected' | 'ready' | 'offline';
+
+export interface SignalingAdapter extends EventEmitter {
+  status: SignalingAdapterStatus;
+  uri: string;
+
+  setToken(token: string): void;
+  destroy(): void;
+  updatePreferredURI(uri: string | null): void;
+  updateURIs(uris: string[]): void;
+
+  invite(sdp: string, callSid: string, params: string): void;
+  answer(sdp: string, callSid: string): void;
+  hangup(callSid: string, message?: string | null): void;
+  reject(callSid: string): void;
+  reinvite(sdp: string, callSid: string): void;
+  reconnect(sdp: string, callSid: string, reconnectToken: string): void;
+
+  dtmf(callSid: string, digits: string): void;
+  sendMessage(callSid: string, content: string, contentType: string,
+              messageType: string, voiceEventSid: string): void;
+  register(mediaCapabilities: Record<string, any>): void;
+}
+
+export type SignalingAdapterEvent =
+  | 'invite' | 'answer' | 'ringing' | 'hangup' | 'cancel'
+  | 'error' | 'offline' | 'ready' | 'connected' | 'transportClose'
+  | 'transportOpen' | 'ack' | 'message' | 'candidate' | 'presence' | 'roster' | 'close';
