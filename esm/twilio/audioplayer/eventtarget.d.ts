3a5c607b34d47e9c34c58a9dbd2a213461b3faea
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2026-05-06 22:08:12 +0000

    2.18.3-dev

--- esm/twilio/audioplayer/eventtarget.d.ts
+++
@@ -1,7 +0,0 @@
-import { EventEmitter } from 'events';
-export default class EventTarget {
-    private _eventEmitter;
-    addEventListener(name: string, handler: Function): EventEmitter;
-    dispatchEvent(name: string, ...args: any[]): boolean;
-    removeEventListener(name: string, handler: Function): EventEmitter;
-}
