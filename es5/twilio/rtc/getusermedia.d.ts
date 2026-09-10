7ef20933768f617707ba2f817b7a0c230d8fe194
twilio-vblocks-ci <svc.vblocks-ci@twilio.com>
2026-09-10 22:28:43 +0000

    2.18.6-dev

--- es5/twilio/rtc/getusermedia.d.ts
+++
@@ -1,2 +0,0 @@
-declare function getUserMedia(constraints: any, options: any): Promise<unknown>;
-export default getUserMedia;
--- esm/twilio/rtc/rtcpc.d.ts
+++
@@ -1,7 +0,0 @@
-declare function RTCPC(options: {
-    RTCPeerConnection?: any;
-}): void;
-declare namespace RTCPC {
-    var test: () => boolean;
-}
-export default RTCPC;
